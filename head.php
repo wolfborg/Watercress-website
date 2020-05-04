71fa23bb53e870c51cd3427fb76b35beffbb3bbe
Derek Chaplin <wolfborg@users.noreply.github.com>
2020-05-04 14:16:27 -0400

    Cleaned up directory, adjusted format, and fixed header loading

---
+++ head.php
@@ -0,0 +1,60 @@
+<head>
+    <title>Watercress Studios</title>
+    <!--    This is the icon stuff -->
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="img/logo/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="img/logo/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="img/logo/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="img/logo/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="img/logo/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="img/logo/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="img/logo/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="img/logo/apple-touch-icon-180x180.png" />
+      
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <meta name="viewport" content="width=device-width"/>
+    <meta charset="UTF-8">
+    <!-- Keep an eye out on this line of code, in case scripts act funny. -->
+    <meta http-equiv="Content-Security-Policy" content="default-src https:; script-src https: 'unsafe-inline'; style-src https: 'unsafe-inline'">
+    <meta name="description" content="We are the largest freeware Visual Novel studio in the US!"/>
+    <meta name="keywords" content="Visual Novels, EVN, OELVN,"/>
+    <meta name="author" content="Happiness+"/>
+    <!-- Open graph meta tags -->
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Watercress"/>
+    <meta property="og:description" content="We are the largest freeware Visual Novel studio in the US!"/>
+    <meta property="og:url" content="https://watercressstudios.com/"/>
+    <meta property="og:image" content="https://watercressstudios.com/logo.png" />
+    <meta property="og:site_name" content="Watercress" />
+    <!-- Twitter Card  stuff, will fix up with a better image later-->
+    <!-- <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:title" content="Watercress Studios"/>
+    <meta name="twitter:description" content="Our website"/>
+    <meta name="twitter:site" content="@TeamWatercress"/>
+    <meta name="twitter:image" content="http://www.yoursite.com/yourimage.jpg"/>
+    <meta name="twitter:creator" content="@Happinessplus__"/> -->
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Didact+Gothic|Lato&display=swap" rel="stylesheet"> 
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+    <script>
+    $(document).ready(function() {
+        $(".open-nav").click(function() {
+            $(".navigation").toggleClass("show");
+    		$(".open-nav").toggleClass("active-green");
+        });
+    	$(".open-project").click(function() {
+            $(".project-subblock").toggleClass("open");
+    		$(".project-hover").toggleClass("open");
+        });
+    });
+
+    </script>
+</head>
+
+<body>
+<div class="container">
--- header.php
+++ header.php
@@ -1,67 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Watercress Studios</title>
-<!--    This is the icon stuff -->
-<link rel="icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
-  
-<link rel="stylesheet" type="text/css" href="styles.css" />
-    <meta name="viewport" content="width=device-width"/>
-    <meta charset="UTF-8">
-    <!-- Keep an eye out on this line of code, in case scripts act funny. -->
-    <meta http-equiv="Content-Security-Policy" content="default-src https:; script-src https: 'unsafe-inline'; style-src https: 'unsafe-inline'">
-    <meta name="description" content="We are the largest freeware Visual Novel studio in the US!"/>
-    <meta name="keywords" content="Visual Novels, EVN, OELVN,"/>
-    <meta name="author" content="Happiness+"/>
-    <!-- Open graph meta tags -->
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Watercress"/>
-    <meta property="og:description" content="We are the largest freeware Visual Novel studio in the US!"/>
-    <meta property="og:url" content="https://watercressstudios.com/"/>
-    <meta property="og:image" content="https://watercressstudios.com/logo.png" />
-    <meta property="og:site_name" content="Watercress" />
-    <!-- Twitter Card  stuff, will fix up with a better image later-->
-    <!-- <meta name="twitter:card" content="summary_large_image"/>
-    <meta name="twitter:title" content="Watercress Studios"/>
-    <meta name="twitter:description" content="Our website"/>
-    <meta name="twitter:site" content="@TeamWatercress"/>
-    <meta name="twitter:image" content="http://www.yoursite.com/yourimage.jpg"/>
-    <meta name="twitter:creator" content="@Happinessplus__"/> -->
-
-<link href="https://fonts.googleapis.com/css?family=Nunito:200,400" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Didact+Gothic|Lato&display=swap" rel="stylesheet"> 
-
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
-<script
-  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
-
-
-
-<script>
-$(document).ready(function() {
-    $(".open-nav").click(function() {
-        $(".navigation").toggleClass("show");
-		$(".open-nav").toggleClass("active-green");
-    });
-	$(".open-project").click(function() {
-        $(".project-subblock").toggleClass("open");
-		$(".project-hover").toggleClass("open");
-    });
-});
-
-</script>
-</head>
-
-<body>
-<div class="container">
+<div class="mainBranding">
+	<a href="index.php"><img src="img/logo/logo.png" id="logo" alt="Watercress Studios" name="Watercress Studios" /></a>
+	<a id="companyName" href="index.php"><h1>Watercress</h1></a>
+</div>
+<div class="navigation-box">
+	<a href="javascript:void(0);" class="open-nav"><i class="fas fa-bars"></i> Navigation</a>
+	<div class="navigation">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="games.php">Our Games</a></li>
+			<li><a href="about.php">About Us</a></li>
+		</ul>
+	</div> 
+	<div class="socialmedia-navigation">
+		<ul>
+			<li><a href="https://discord.gg/watercress" target="_blank" class="fab fa-discord"></a></li>
+            <li><a href="https://twitter.com/TeamWatercress" target="_blank" class="fab fa-twitter"></a></li>
+            <li><a href="http://twitch.tv/watercressstudios" target="_blank" class="fab fa-twitch"></a></li>
+		</ul>
+	</div>
+</div>
